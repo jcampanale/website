896c1c3cdd544895f1dafb4602cba69107c24cf0
JCampanale <jcampanale@wpi.edu>
2021-12-22 13:38:54 -0500

    fix image

--- build/project-page.js
+++ build/project-page.js
@@ -12,7 +12,8 @@ class Projects extends React.Component {
       src: "./images/memory.jpg",
       width: "405",
       height: "200",
-      className: "projImg"
+      className: "projImg",
+      alt: "Simon Says Memory Game Image"
     }), /* @__PURE__ */ React.createElement("div", {
       class: "card-container"
     }, /* @__PURE__ */ React.createElement("div", {
@@ -28,7 +29,8 @@ class Projects extends React.Component {
       src: "./images/tale.jpg",
       width: "405",
       height: "200",
-      className: "projImg"
+      className: "projImg",
+      alt: "Tale of the Sword Guy Image"
     }), /* @__PURE__ */ React.createElement("div", {
       class: "card-container"
     }, /* @__PURE__ */ React.createElement("div", {
@@ -44,13 +46,31 @@ class Projects extends React.Component {
       src: "./images/conway.png",
       width: "405",
       height: "200",
-      className: "projImg"
+      className: "projImg",
+      alt: "John Conways Game of Life Image"
     }), /* @__PURE__ */ React.createElement("div", {
       class: "card-container"
     }, /* @__PURE__ */ React.createElement("div", {
       className: "containerItems"
     }, /* @__PURE__ */ React.createElement("h2", null, "John Conway's Game of", /* @__PURE__ */ React.createElement("br", null), " Life"), /* @__PURE__ */ React.createElement("br", null), /* @__PURE__ */ React.createElement("ul", null, /* @__PURE__ */ React.createElement("li", null, "Built a function version of John Conway's ", /* @__PURE__ */ React.createElement("br", null), "game of life in Linux using C++ using a ", /* @__PURE__ */ React.createElement("br", null), "mailbox system between threads"), /* @__PURE__ */ React.createElement("li", null, "Used Semaphores as a synchronization ", /* @__PURE__ */ React.createElement("br", null), "mechanism")), /* @__PURE__ */ React.createElement("br", null), /* @__PURE__ */ React.createElement("p", null, "Note the image used is not my own and can", /* @__PURE__ */ React.createElement("br", null), " be found ", /* @__PURE__ */ React.createElement("a", {
       href: "https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life",
+      class: "vidLink"
+    }, "here")), /* @__PURE__ */ React.createElement("br", null))))), /* @__PURE__ */ React.createElement("div", {
+      className: "items"
+    }, /* @__PURE__ */ React.createElement("div", {
+      class: "card"
+    }, /* @__PURE__ */ React.createElement("img", {
+      src: "./images/overlay.jpg",
+      width: "405",
+      height: "200",
+      className: "projImg",
+      alt: "Overlay Network Image"
+    }), /* @__PURE__ */ React.createElement("div", {
+      class: "card-container"
+    }, /* @__PURE__ */ React.createElement("div", {
+      className: "containerItems"
+    }, /* @__PURE__ */ React.createElement("h2", null, "ASCII Text Game Engine", /* @__PURE__ */ React.createElement("br", null), " + Games"), /* @__PURE__ */ React.createElement("br", null), /* @__PURE__ */ React.createElement("ul", null, /* @__PURE__ */ React.createElement("li", null, "Developed a fully functional game engine ", /* @__PURE__ */ React.createElement("br", null), "with ASCII Text based graphics using C++"), /* @__PURE__ */ React.createElement("li", null, "Utilized Simple and Fast Multimedia Library ", /* @__PURE__ */ React.createElement("br", null), "for sound, and Visual Studio for window display"), /* @__PURE__ */ React.createElement("li", null, "Worked with a partner to develop and release ", /* @__PURE__ */ React.createElement("br", null), "a game to our classmates")), /* @__PURE__ */ React.createElement("br", null), /* @__PURE__ */ React.createElement("p", null, "A video trailer of the final game can be found", /* @__PURE__ */ React.createElement("br", null), " ", /* @__PURE__ */ React.createElement("a", {
+      href: "https://www.youtube.com/watch?v=ww84Fs2NXpw",
       class: "vidLink"
     }, "here")), /* @__PURE__ */ React.createElement("br", null)))))));
   }
